13bf78ee14b936eaca8cea18fbe70d20096bd17a
Apollo3zehn <git@m1.apollo3zehn.net>
2022-09-30 14:24:07 +0200

    Improvements.

--- tests/Nexus.Writers.Mat73.Tests/Mat73Tests.cs
+++ tests/Nexus.Writers.Mat73.Tests/Mat73Tests.cs
@@ -31,8 +31,8 @@ namespace Nexus.Writers.Tests
             var begin = new DateTime(2020, 01, 01, 0, 0, 0, DateTimeKind.Utc);
             var samplePeriod = TimeSpan.FromSeconds(1);
 
-            var catalogItems = _fixture.Catalogs.SelectMany(catalog => catalog.Resources
-                .SelectMany(resource => resource.Representations.Select(representation => new CatalogItem(catalog, resource, representation))))
+            var catalogItems = _fixture.Catalogs.SelectMany(catalog => catalog.Resources!
+                .SelectMany(resource => resource.Representations!.Select(representation => new CatalogItem(catalog, resource, representation))))
                 .ToArray();
 
             var random = new Random(Seed: 1);
@@ -83,23 +83,23 @@ namespace Nexus.Writers.Tests
             // catalog 1
             var catalog1 = h5File.Group("A_B_C");
             var resources1 = _fixture.Catalogs[0].Resources;
-            Assert.Equal(resources1.Count + 1, catalog1.Children.Count());
-            var representations1 = _fixture.Catalogs[0].Resources.SelectMany(resource => resource.Representations).ToList();
+            Assert.Equal(resources1!.Count + 1, catalog1.Children.Count());
+            var representations1 = _fixture.Catalogs[0].Resources!.SelectMany(resource => resource.Representations!).ToList();
             Assert.Equal(representations1.Count, catalog1.Group("resource1").Children.Count());
             // implement when HDF5.NET is able to read any struct easily
-            //AssertProperties(_fixture.Catalogs[0].Properties, catalog1.PropertyInfo.Properties);
-            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Channels[0].PropertyInfo.Properties);
-            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Channels[1].PropertyInfo.Properties);
+            //AssertProperties(_fixture.Catalogs[0].Properties, catalog1.Properties);
+            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Resources[0].Properties);
+            //AssertProperties(_fixture.Catalogs[0].Resources[0].Properties, catalog1.Resources[1].Properties);
 
             // catalog 2
             var catalog2 = h5File.Group("D_E_F");
             var resources2 = _fixture.Catalogs[0].Resources;
-            Assert.Equal(resources2.Count + 1, catalog2.Children.Count());
-            var representations2 = _fixture.Catalogs[1].Resources.SelectMany(resource => resource.Representations).ToList();
+            Assert.Equal(resources2!.Count + 1, catalog2.Children.Count());
+            var representations2 = _fixture.Catalogs[1].Resources!.SelectMany(resource => resource.Representations!).ToList();
             Assert.Equal(representations2.Count, catalog2.Group("resource3").Children.Count());
             // implement when HDF5.NET is able to read any struct easily
-            //AssertProperties(_fixture.Catalogs[1].Properties, catalog2.PropertyInfo.Properties);
-            //AssertProperties(_fixture.Catalogs[1].Resources[0].Properties, catalog2.Channels[0].PropertyInfo.Properties);
+            //AssertProperties(_fixture.Catalogs[1].Properties, catalog2.Properties);
+            //AssertProperties(_fixture.Catalogs[1].Resources[0].Properties, catalog2.Resources[0].Properties);
 
             //void AssertProperties(IReadOnlyDictionary<string, string> expected, List<xxx> actual)
             //{
